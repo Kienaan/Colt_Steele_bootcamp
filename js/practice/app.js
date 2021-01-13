f6ff2a6e4f678ef514c3f4e050d4f478532c0c43
Kienaan <69430084+Kienaan@users.noreply.github.com>
2021-01-13 15:41:04 -0600

    update

---
+++ js/practice/app.js
@@ -0,0 +1,51 @@
+// const age = 8;
+
+// if (age < 5) {
+//     console.log("You are a baby. You get in for free")
+// }
+// else if (age < 10) {
+//     console.log("You are a child> You pay $10")
+// }
+// else if (age < 65) {
+//     console.log("You are an adult. You pay $20")
+// }
+
+// function getColor(phrase) {
+//     //WRITE YOUR CODE BETWEEN THIS LINE: ↓ ↓ ↓ ↓ 
+//     if (phrase === "stop") {
+//         console.log("red");
+//     }
+//     else if (phrase === "slow") {
+//         console.log("yellow");
+//     }
+//     else if (phrase === "go") {
+//         console.log("green");
+//     }
+//     else {
+//         console.log("purple");
+//     }
+//     //AND THIS LINE ↑↑↑↑↑
+// }
+
+// const password = prompt("please enter a new password");
+
+// // password must be 6+ characters
+// if (password.length >= 6) {
+//     if (password.indexOf(' ') === -1) {
+//         console.log("Valid Password!");
+//     } else {
+//         console.log("Password cannot contain spaces!")
+//     }
+// } else {
+//     console.log("Password   too short")
+// }
+
+// const password = prompt("please enter a new password");
+
+// if (password.length >= 6 && password.indexOf(' ') === -1) {
+//     console.log("Valid password!");
+// }
+// else {
+//     console.log("Incorrect format for password!");
+// }
+
