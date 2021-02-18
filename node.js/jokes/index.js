63a91401b559541759b3eb8f30d5e41e350dc230
Kienaan <69430084+Kienaan@users.noreply.github.com>
2021-02-18 15:29:26 -0600

    Express

--- node.js/jokes/index.js
+++ node.js/jokes/index.js
@@ -1,5 +1,6 @@
 const joke = require("give-me-a-joke");
+const colors = require("colors");
 console.dir(joke);
 joke.getRandomDadJoke(function (joke) {
-    console.log(joke);
+    console.log(joke.);
 })
